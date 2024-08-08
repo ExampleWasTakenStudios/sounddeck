bf39eff5097a328a6142caabde8fd8f296fce054
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-09 01:54:52 +0200

    add header

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,9 @@
+import { Header } from './components/header/Header';
+
 export const Root = () => {
   return (
-    <div className="bg-black h-screen w-screen">
-      
+    <div className="bg-black text-white h-screen w-screen">
+      <Header />
     </div>
   );
 };
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <header className="h-14 flex justify-center items-center shadow shadow-green">
+      <h1 className="text-2xl font-thin">
+        <Link to="/">SoundDeck</Link>
+      </h1>
+    </header>
+  );
+};
