4733858a57037b6aa2e48b585362fb284f7dc04b
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-09 19:18:34 +0200

    login route

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,10 +3,10 @@ import { Header } from './components/header/Header';
 
 export const Root = () => {
   return (
-    <div className="bg-black text-white font-sans-serif min-h-screen min-w-screen flex flex-col">
+    <div className="bg-black text-white font-sans-serif min-h-screen min-w-screen h-screen w-screen flex flex-col">
       <Header />
 
-      <main className="m-4 flex flex-col gap-3 mb-14">
+      <main className="h-full m-4 flex flex-col gap-3 mb-14">
         <Outlet />
       </main>
     </div>
