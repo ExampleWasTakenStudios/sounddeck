59977dbf29b9df9fc9f11bcad94cfab70288c7b1
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-09 03:03:00 +0200

    add secondary button

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,20 @@
 import { PrimaryButton } from './components/button/PrimaryButton';
+import { SecondaryButton } from './components/button/SecondaryButton';
 import { Header } from './components/header/Header';
 
 export const Root = () => {
   return (
     <div className="bg-black text-white font-sans-serif h-screen w-screen flex flex-col">
       <Header />
-      <main className="m-4 flex justify-center">
+      <main className="m-4">
         <PrimaryButton
           content="Login with Spotify"
           icon={<img className="w-4 aspect-square" src="/img/spotify/icons/Spotify_Icon_Black.png"></img>}
+          onClick={() => console.log('login button clicked')}
+        />
+        <SecondaryButton
+          content="Login with Spotify"
+          icon={<img className="w-4 aspect-square" src="/img/spotify/icons/Spotify_Icon_White.png"></img>}
           onClick={() => console.log('login button clicked')}
         />
       </main>
--- src/components/button/SecondaryButton.tsx
+++ src/components/button/SecondaryButton.tsx
@@ -1,18 +1,18 @@
 import clsx from 'clsx';
 import { ReactNode } from 'react';
 
-interface PrimaryButtonProps {
+interface SecondaryButtonProps {
   content: string;
   icon?: ReactNode;
   disabled?: boolean;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const PrimaryButton = ({ content, icon, disabled, onClick }: PrimaryButtonProps) => {
+export const SecondaryButton = ({ content, icon, disabled, onClick }: SecondaryButtonProps) => {
   const css = clsx(
-    'bg-green text-white px-3 py-2 rounded flex flex-row justify-center items-center gap-1 text-center cursor-pointer',
-    disabled && 'opacity-75 cursor-not-allowed active:bg-green',
-    !disabled && 'active:bg-green/70'
+    'bg-purple text-white px-3 py-2 rounded flex flex-row justify-center items-center gap-1 text-center cursor-pointer',
+    disabled && 'opacity-75 cursor-not-allowed active:bg-purple',
+    !disabled &&  'active:bg-purple/70',
   );
 
   return (
