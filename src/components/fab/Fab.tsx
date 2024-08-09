06daaa96e73489196b728dbdcca6d55018d616ee
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-09 06:28:55 +0200

    active state for fab

--- src/components/fab/Fab.tsx
+++ src/components/fab/Fab.tsx
@@ -8,7 +8,7 @@ interface FabProps {
 export const Fab = ({ onClick, children }: FabProps) => {
   return (
     <div
-      className="fixed bottom-16 right-4 w-14 aspect-square flex justify-center items-center drop-shadow bg-green text-black rounded-2xl cursor-pointer"
+      className="fixed bottom-16 right-4 w-14 aspect-square flex justify-center items-center drop-shadow bg-green text-black rounded-2xl cursor-pointer active:bg-green/70"
       onClick={onClick}
     >
       {children}
