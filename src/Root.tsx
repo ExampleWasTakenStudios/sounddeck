3f933a668da1849ecb45875881c754d6b36c4879
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-09 04:00:35 +0200

    playlist card

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,7 @@ import { PrimaryButton } from './components/button/PrimaryButton';
 import { SecondaryButton } from './components/button/SecondaryButton';
 import { Header } from './components/header/Header';
 import { Navbar } from './components/navbar/Navbar';
+import { PlaylistCard } from './components/playlist-card/PlaylistCard';
 import { MainSearchBar } from './components/searchbar/MainSearchBar';
 
 export const Root = () => {
@@ -9,19 +10,36 @@ export const Root = () => {
     <div className="bg-black text-white font-sans-serif h-screen w-screen flex flex-col">
       <Header />
 
-      <main className="m-4">
-        <PrimaryButton
-          content="Login with Spotify"
-          icon={<img className="w-4 aspect-square" src="/img/spotify/icons/Spotify_Icon_Black.png"></img>}
-          onClick={() => console.log('login button clicked')}
-        />
-        <SecondaryButton
-          content="Login with Spotify"
-          icon={<img className="w-4 aspect-square" src="/img/spotify/icons/Spotify_Icon_White.png"></img>}
-          onClick={() => console.log('login button clicked')}
-        />
+      <main className="m-4 flex flex-col gap-3">
+        <div>
+          <PrimaryButton
+            content="Login with Spotify"
+            icon={<img className="w-4 aspect-square" src="/img/spotify/icons/Spotify_Icon_Black.png"></img>}
+            onClick={() => console.log('login button clicked')}
+          />
+        </div>
+        <div>
+          <SecondaryButton
+            content="Login with Spotify"
+            icon={<img className="w-4 aspect-square" src="/img/spotify/icons/Spotify_Icon_White.png"></img>}
+            onClick={() => console.log('login button clicked')}
+          />
+        </div>
 
-        <MainSearchBar onChange={(event) => console.log(event.target.value)} />
+        <div>
+          <MainSearchBar onChange={(event) => console.log(event.target.value)} />
+        </div>
+
+        <div className="flex flex-row gap-5">
+          <PlaylistCard
+            title="Taylor Swift | The Eras Tour Official Setlist"
+            coverPath="/img/mock/playlist_cover.jpg"
+          />
+          <PlaylistCard
+            title="Taylor Swift | The Eras Tour Official Setlist"
+            coverPath="/img/mock/playlist_cover.jpg"
+          />
+        </div>
       </main>
 
       <Navbar currentItem="search" />
