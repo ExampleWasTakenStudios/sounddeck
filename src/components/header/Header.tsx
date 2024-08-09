754e9c2596f8b91b95b424e90150a086b4b20b1a
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-09 04:54:54 +0200

    fix header at the top

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <header className="h-14 min-h-14 flex justify-center items-center shadow shadow-green">
+    <header className="sticky top-0 h-14 min-h-14 w-full flex justify-center items-center bg-black shadow shadow-green">
       <h1 className="text-2xl font-thin">
         <Link to="/">SoundDeck</Link>
       </h1>
