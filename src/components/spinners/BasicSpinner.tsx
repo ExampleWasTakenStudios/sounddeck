08ec53bed81ce6e406d12f2dd2b51b39304c8fe8
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-09 23:26:30 +0200

    add basic spinner

---
+++ src/components/spinners/BasicSpinner.tsx
@@ -0,0 +1,8 @@
+export const BasicSpinner = () => {
+  return (
+    <div>
+      <div className="relative z-10 top-0 left-0 w-10 aspect-square rounded-full border-4 border-green/10"></div>
+      <div className="relative z-20 -top-10 left-0 w-10 aspect-square rounded-full border-l-4 border-t-4 border-green animate-spin"></div>
+    </div>
+  );
+};
