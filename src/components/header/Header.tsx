359fc509db955aeb9a6a1224921a6a8986c57e99
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-09 03:25:50 +0200

    fix header height

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <header className="h-14 flex justify-center items-center shadow shadow-green">
+    <header className="h-14 min-h-14 flex justify-center items-center shadow shadow-green">
       <h1 className="text-2xl font-thin">
         <Link to="/">SoundDeck</Link>
       </h1>
