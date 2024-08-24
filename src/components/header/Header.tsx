07e3de4f7e694cf1246238a83ea592735ed5445d
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-24 21:00:23 +0200

    fix: header not always on top (#15)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 h-14 min-h-14 w-full flex justify-center items-center bg-black shadow shadow-green">
+    <header className="sticky top-0 z-[9999] h-14 min-h-14 w-full flex justify-center items-center bg-black shadow shadow-green">
       <h1 className="text-2xl font-thin">
         <Link to="/">SoundDeck</Link>
       </h1>
