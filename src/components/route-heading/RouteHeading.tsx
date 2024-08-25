014453971f285d5c9bfb8e9be88afb37605f9f9f
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-25 17:45:44 +0200

    add user pp

--- src/components/route-heading/RouteHeading.tsx
+++ src/components/route-heading/RouteHeading.tsx
@@ -1,7 +1,19 @@
 export interface RouteHeadingProps {
   title: string;
+  userProfilePictureUrl: string;
 }
 
-export const RouteHeading = ({ title }: RouteHeadingProps) => {
-  return <h1 className="my-5 text-2xl font-thin">{title}</h1>;
+export const RouteHeading = ({ title, userProfilePictureUrl }: RouteHeadingProps) => {
+  return (
+    <div className="w-full flex flex-row justify-between items-center">
+      <h1 className="my-5 text-2xl font-thin">{title}</h1>
+      <img
+        className="w-[30px] h-[30px] rounded-full aspect-square"
+        src={userProfilePictureUrl}
+        alt="Your Profile Picture"
+        width={10}
+        height={10}
+      />
+    </div>
+  );
 };
--- src/routes/search/Search.tsx
+++ src/routes/search/Search.tsx
@@ -1,4 +1,4 @@
-import { FeaturedPlaylists, PartialSearchResult } from '@spotify/web-api-ts-sdk';
+import { FeaturedPlaylists, PartialSearchResult, UserProfile } from '@spotify/web-api-ts-sdk';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar } from '../../components/navbar/Navbar';
@@ -6,13 +6,15 @@ import { PlaylistCard } from '../../components/playlist-card/PlaylistCard';
 import { RouteHeading } from '../../components/route-heading/RouteHeading';
 import { MainSearchBar } from '../../components/searchbars/mainSearchBar/MainSearchBar';
 import { MainSearchSuggestions } from '../../components/searchbars/mainSearchBar/MainSearchSuggestions';
+import { BasicSpinner } from '../../components/spinners/BasicSpinner';
 import { EmptyState } from '../../EmptyState';
 import { useSpotify } from '../../hooks/useSpotify';
 
 export const Search = () => {
+  const spotify = useSpotify();
+  const [currentUser, setCurrentUser] = useState<UserProfile | null>(null);
   const [searchResults, setSearchResults] = useState<Required<Pick<PartialSearchResult, 'playlists'>> | null>(null);
   const [featuredPlaylists, setFeaturedPlaylists] = useState<FeaturedPlaylists | null>(null);
-  const spotify = useSpotify();
 
   const onSearchBarChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value;
@@ -29,45 +31,57 @@ export const Search = () => {
     void (async () => {
       setFeaturedPlaylists(await spotify.browse.getFeaturedPlaylists());
     })();
+
+    void (async () => {
+      setCurrentUser(await spotify.currentUser.profile());
+    })();
   }, [spotify]);
 
   return (
     <>
-      <RouteHeading title="Search" />
+      {currentUser && featuredPlaylists ? (
+        <>
+          <RouteHeading title="Search" userProfilePictureUrl={currentUser?.images[0].url} />
 
-      <MainSearchBar
-        onChange={(event) => void onSearchBarChange(event)}
-        suggestions={searchResults?.playlists?.items.map((playlist) => {
-          return (
-            <Link to={`/playlist/${playlist.id}`} key={playlist.id}>
-              <MainSearchSuggestions
-                title={playlist.name}
-                artists={playlist.owner.display_name}
-                coverUrl={playlist.images[0].url}
-              />
-            </Link>
-          );
-        })}
-      />
+          <MainSearchBar
+            onChange={(event) => void onSearchBarChange(event)}
+            suggestions={searchResults?.playlists?.items.map((playlist) => {
+              return (
+                <Link to={`/playlist/${playlist.id}`} key={playlist.id}>
+                  <MainSearchSuggestions
+                    title={playlist.name}
+                    artists={playlist.owner.display_name}
+                    coverUrl={playlist.images[0].url}
+                  />
+                </Link>
+              );
+            })}
+          />
 
-      <h2 className="text-xl font-thin my-2">Featured Playlists</h2>
+          <h2 className="text-xl font-thin my-2">Featured Playlists</h2>
 
-      {featuredPlaylists ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-5">
-          {featuredPlaylists.playlists.items.map((playlist) => {
-            return (
-              <Link to={`/playlist/${playlist.id}`} key={playlist.id}>
-                <PlaylistCard
-                  title={playlist.name}
-                  width={playlist.images[0].width}
-                  coverUrl={playlist.images[0].url}
-                />
-              </Link>
-            );
-          })}
-        </div>
+          {featuredPlaylists ? (
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-5">
+              {featuredPlaylists.playlists.items.map((playlist) => {
+                return (
+                  <Link to={`/playlist/${playlist.id}`} key={playlist.id}>
+                    <PlaylistCard
+                      title={playlist.name}
+                      width={playlist.images[0].width}
+                      coverUrl={playlist.images[0].url}
+                    />
+                  </Link>
+                );
+              })}
+            </div>
+          ) : (
+            <EmptyState content="We'd love to recommend you something good but we're just as clueless as you." />
+          )}
+        </>
       ) : (
-        <EmptyState content="We'd love to recommend you something good but we're just as clueless as you." />
+        <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center">
+          <BasicSpinner />
+        </div>
       )}
 
       <Navbar />
