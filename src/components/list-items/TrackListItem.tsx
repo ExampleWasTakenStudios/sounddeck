e5de5198b8106ea9b09bc4380f9572a27cbbfec0
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-25 02:58:07 +0200

    fix image and explicit tag sizes

--- src/components/list-items/TrackListItem.tsx
+++ src/components/list-items/TrackListItem.tsx
@@ -14,12 +14,14 @@ export const TrackListItem = ({ title, artist, explicit, coverUrl }: TrackListIt
         className="h-14 aspect-square object-cover rounded-sm"
         src={coverUrl}
         alt={`Cover of ${title} by ${artist}.`}
+        width={56}
+        height={56}
       />
       <div className="w-full truncate flex flex-col justify-between">
         <h1 className="text-white truncate">{title}</h1>
         <div className="flex flex-row items-center gap-1">
-          {explicit && <CircleAlert className="text-subdued" size={16} />}
-          <h2 className="font-light text-subdued">{artist}</h2>
+          {explicit && <CircleAlert className="text-subdued min-w-[16px] min-h-[16px]" size={16} />}
+          <h2 className="font-light text-subdued truncate">{artist}</h2>
         </div>
       </div>
     </div>
