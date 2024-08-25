f3b20f895a9b4dec41e49de8efa095a2c692f1a3
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-08-25 18:48:21 +0200

    add logout option

--- src/components/route-heading/RouteHeading.tsx
+++ src/components/route-heading/RouteHeading.tsx
@@ -1,19 +1,45 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSpotify } from '../../hooks/useSpotify';
+
 export interface RouteHeadingProps {
   title: string;
   userProfilePictureUrl: string;
 }
 
 export const RouteHeading = ({ title, userProfilePictureUrl }: RouteHeadingProps) => {
+  const spotify = useSpotify();
+  const navigate = useNavigate();
+  const [userSettingsOpen, setUserSettingsOpen] = useState(false);
+
   return (
-    <div className="w-full flex flex-row justify-between items-center">
-      <h1 className="my-5 text-2xl font-thin">{title}</h1>
-      <img
-        className="w-[30px] h-[30px] rounded-full aspect-square"
-        src={userProfilePictureUrl}
-        alt="Your Profile Picture"
-        width={10}
-        height={10}
-      />
-    </div>
+    <>
+      <div className="w-full flex flex-row justify-between items-center">
+        <h1 className="my-5 text-2xl font-thin">{title}</h1>
+        <div className="relative">
+          <img
+            className="w-[30px] h-[30px] rounded-full aspect-square"
+            src={userProfilePictureUrl}
+            alt="Your Profile Picture"
+            width={30}
+            height={30}
+            onClick={() => setUserSettingsOpen(!userSettingsOpen)}
+          />
+          {userSettingsOpen && (
+            <div className="absolute z-10 top-[30px] right-0 w-28 p-1 bg-gray rounded">
+              <div
+                className="w-full h-10 rounded-sm p-1 flex items-center hover:bg-light-gray"
+                onClick={() => {
+                  spotify.logOut();
+                  navigate('/login');
+                }}
+              >
+                <p>Log out</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ export default {
     extend: {
       colors: {
         black: '#121212',
+        gray: '#282828',
+        'light-gray': '#3E3E3E',
         white: '#FFFFFF',
         green: '#1DB954',
         purple: '#400073',
