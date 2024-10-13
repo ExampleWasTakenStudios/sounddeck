05a5a2480ac3771370d685cfcd4a8290be3c24e8
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-10-13 17:52:42 +0200

    library and playlist routes desktop support

--- src/components/list-items/TrackListItem.tsx
+++ src/components/list-items/TrackListItem.tsx
@@ -9,7 +9,7 @@ interface TrackListItemProps {
 
 export const TrackListItem = ({ title, artist, explicit, coverUrl }: TrackListItemProps) => {
   return (
-    <div className="w-full flex flex-row items-center gap-2 sm:p-[0.375rem] transition-all ease-out hover:bg-subdued/20 rounded active:scale-95">
+    <div className="cursor-default w-full flex flex-row items-center gap-2 transition-all ease-out rounded">
       <img
         className="h-14 aspect-square object-cover rounded-sm"
         src={coverUrl}
