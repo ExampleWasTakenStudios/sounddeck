2f372a5cdbc5b4d7a7d57c0db3bcf949eab7916c
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-10-13 16:21:04 +0200

    feat: desktop capable landingpage (#47)
    * remove dev root index route
    
    * link header to search route
    
    * login route desktop capable
    
    * remove borders

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 z-[9999] h-14 min-h-14 w-full flex justify-center items-center bg-black shadow shadow-green">
       <h1 className="text-2xl font-thin">
-        <Link to="/">SoundDeck</Link>
+        <Link to="/search">SoundDeck</Link>
       </h1>
     </header>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AuthGuard } from './AuthGuard';
 import { ErrorPage } from './ErrorPage';
 import { Root } from './Root';
-import { RootIndex } from './routes/RootIndex';
 import { Playlist } from './routes/[playlist]/Playlist';
 import { SavePlaylist } from './routes/[playlist]/SavePlaylist';
 import { Library } from './routes/library/Libary';
@@ -15,14 +14,14 @@ import './tailwind.css';
 
 const router = createBrowserRouter([
   {
+    index: true,
+    element: <Login />,
+  },
+  {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        index: true,
-        element: <RootIndex />,
-      },
       {
         element: <AuthGuard />,
         children: [
