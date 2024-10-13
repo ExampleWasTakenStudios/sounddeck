de864294d5a529735f8c49c6674dd0db25a060d5
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-10-13 18:16:35 +0200

    add label to navbar items

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,12 +10,14 @@ export const Navbar = () => {
   const iconSize = 26;
 
   return (
-    <nav className="backdrop-blur bg-black/50 h-14 w-screen fixed bottom-0 left-0 flex flex-row justify-around items-center">
-      <Link to="/search">
+    <nav className="backdrop-blur bg-black/50 h-20 w-screen fixed bottom-0 left-0 flex flex-row justify-around items-center">
+      <Link to="/search" className="flex flex-col justify-center items-center">
         <Search size={iconSize} className={clsx(currentItem === 'search' ? 'stroke-white' : 'text-subdued')} />
+        <p className={clsx(currentItem === 'search' ? 'text-white' : 'text-subdued')}>Search</p>
       </Link>
-      <Link to="/library">
+      <Link to="/library" className="flex flex-col justify-center items-center">
         <Library size={iconSize} className={clsx(currentItem === 'library' ? 'stroke-white' : 'text-subdued')} />
+        <p className={clsx(currentItem === 'library' ? 'text-white' : 'text-subdued')}>Library</p>
       </Link>
     </nav>
   );
