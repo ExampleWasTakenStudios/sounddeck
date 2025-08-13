d753758a0089f5493a385cee1ead0e8c40c41dc0
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2025-08-13 23:07:55 +0200

    visual fixes and log removal

--- src/Root.tsx
+++ src/Root.tsx
@@ -6,7 +6,7 @@ export const Root = () => {
     <div className="bg-black text-white font-sans-serif min-h-screen min-w-screen h-full w-full flex flex-col">
       <Header />
 
-      <main className="h-full m-4 flex flex-col gap-3 mb-14">
+      <main className="h-full m-4 flex flex-col gap-3 mb-20">
         <Outlet />
       </main>
     </div>
