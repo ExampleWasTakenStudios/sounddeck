955dc960b3c8bf65f010f034c527e0b48e3d6cc6
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2025-08-15 00:23:30 +0200

    use white text on orange background

--- src/components/button/PrimaryButton.tsx
+++ src/components/button/PrimaryButton.tsx
@@ -3,7 +3,7 @@ import { IButtonProps } from './IButtonProps';
 
 export const PrimaryButton = ({ content, type, icon, disabled, width, height, onClick }: IButtonProps) => {
   const css = clsx(
-    'bg-primary text-true-black px-3 py-2 rounded flex flex-row justify-center items-center gap-1 text-center cursor-pointer',
+    'bg-primary text-white px-3 py-2 rounded flex flex-row justify-center items-center gap-1 text-center cursor-pointer',
     disabled && 'opacity-75 cursor-not-allowed active:bg-primary',
     !disabled && 'active:bg-primary/70',
   );
