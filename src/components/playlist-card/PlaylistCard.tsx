80a33d4caa1c203e938df46020151e2669fc61a0
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2025-08-14 23:54:11 +0200

    add featured item to search route

--- src/components/playlist-card/PlaylistCard.tsx
+++ src/components/playlist-card/PlaylistCard.tsx
@@ -1,17 +1,22 @@
+import { Image } from '@spotify/web-api-ts-sdk';
+import { useOptimalImage } from '../../hooks/useOptimalImage';
+
 interface PlaylistCardProps {
   title: string;
   width: number;
-  coverUrl: string;
+  covers: Image[];
 }
 
-export const PlaylistCard = ({ title, width, coverUrl }: PlaylistCardProps) => {
+export const PlaylistCard = ({ title, width, covers }: PlaylistCardProps) => {
+  const getOptimalImage = useOptimalImage();
+
   return (
-    <div className="flex flex-col gap-1 aspect-square w-full transition-transform ease-out active:scale-95">
+    <div className="border flex flex-col gap-1 aspect-square w-full transition-transform ease-out active:scale-95">
       <img
         width={width}
         height={width}
         className="rounded-md aspect-square"
-        src={coverUrl}
+        src={getOptimalImage(covers, width, width).url}
         alt="Playlist cover"
         loading="lazy"
       />
--- src/routes/search/Search.tsx
+++ src/routes/search/Search.tsx
@@ -1,17 +1,24 @@
-import { SimplifiedPlaylist, UserProfile } from '@spotify/web-api-ts-sdk';
+import { Playlist, SimplifiedPlaylist, UserProfile } from '@spotify/web-api-ts-sdk';
 import { ChangeEvent, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Navbar } from '../../components/navbar/Navbar';
 import { RouteHeading } from '../../components/route-heading/RouteHeading';
 import { MainSearchBar } from '../../components/searchbars/mainSearchBar/MainSearchBar';
 import { MainSearchSuggestions } from '../../components/searchbars/mainSearchBar/MainSearchSuggestions';
 import { BasicSpinner } from '../../components/spinners/BasicSpinner';
-import { EmptyState } from '../../EmptyState';
 import { useSpotify } from '../../hooks/useSpotify';
 
 export const Search = () => {
   const spotify = useSpotify();
   const [currentUser, setCurrentUser] = useState<UserProfile | null>(null);
   const [searchResult, setSearchResult] = useState<SimplifiedPlaylist[]>();
+  const [featuredItem, setFeaturedItem] = useState<Playlist>();
+
+  useEffect(() => {
+    void (async () => {
+      setFeaturedItem(await spotify.playlists.getPlaylist('65uAjFTt4N8sEJeonhNOBL', undefined));
+    })();
+  }, [spotify]);
 
   useEffect(() => {
     void (async () => {
@@ -62,9 +69,30 @@ export const Search = () => {
             />
           </div>
 
-          <h2 className="text-xl font-thin my-2">Featured Playlists</h2>
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-xl font-thin my-2">Featured</h2>
 
-          <EmptyState content="We'd love to recommend you something good but we're just as clueless as you." />
+            {featuredItem ? (
+              <Link
+                to={featuredItem.external_urls.spotify}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex flex-col items-center"
+              >
+                <img
+                  className="my-2 rounded-md aspect-square"
+                  src={
+                    'https://upload.wikimedia.org/wikipedia/en/a/a4/The_Life_of_a_Showgirl_-_Taylor_Swift_album_artwork.png'
+                  }
+                  height={250}
+                  width={250}
+                />
+                <h1 className="text-center w-1/2">{featuredItem.name}</h1>
+              </Link>
+            ) : (
+              <BasicSpinner />
+            )}
+          </div>
         </>
       ) : (
         <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center">
