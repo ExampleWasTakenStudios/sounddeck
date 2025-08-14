5663165a7215f09a2511e2f2817028b39f3c860f
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2025-08-15 00:05:37 +0200

    feat: featured entry on search route (#53)
    add featured item to search route

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,22 @@ export default {
         subdued: '#AFAFAF',
         'dark-subdued': '#636363',
       },
+      fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      },
+      fontFamily: {
+        body: 'Montserrat',
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
     },
   },
   plugins: [],
